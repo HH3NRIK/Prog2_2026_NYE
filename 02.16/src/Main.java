4f2647ef992c91da642dc08e4e41fd6ca2dcbb2d
Halász Henrik <halaszhenrik1@gmail.com>
2026-02-16 09:41:00 +0100

    push

---
+++ 02.16/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        int szam = 0123;
+        System.out.println("A szám értéke: " + szam);
+        float pi = 3.1415f;
+        System.out.println("Pi = " + pi);
+        char karakter = '\u2713';
+        System.out.println("Karakter = " +  karakter);
+    }
+}
